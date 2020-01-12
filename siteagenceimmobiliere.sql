13e96ac7cc87a5d4432ba8eaf797878f10238b50
StephaneDUPUY <stephane_dupuy@msn.com>
2020-01-12 22:42:55 +0100

    DB update part 2

---
+++ siteagenceimmobiliere.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  Dim 12 jan. 2020 à 21:42
+-- Version du serveur :  5.7.26
+-- Version de PHP :  7.3.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `siteagenceimmobiliere`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `estate`
+--
+
+DROP TABLE IF EXISTS `estate`;
+CREATE TABLE IF NOT EXISTS `estate` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` longtext COLLATE utf8mb4_unicode_ci,
+  `surface` int(11) NOT NULL,
+  `rooms` int(11) NOT NULL,
+  `bedrooms` int(11) NOT NULL,
+  `floor` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `heat` int(11) NOT NULL,
+  `city` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `postal_code` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sold` tinyint(1) NOT NULL DEFAULT '0',
+  `created_at` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `estate`
+--
+
+INSERT INTO `estate` (`id`, `title`, `description`, `surface`, `rooms`, `bedrooms`, `floor`, `price`, `heat`, `city`, `address`, `postal_code`, `sold`, `created_at`) VALUES
+(1, 'Premier bien', 'Quelques informations', 50, 5, 2, 2, 180000, 1, 'Nantes', 'Route de La Jonelière', '44000', 0, '2019-12-19'),
+(2, 'Deuxième bien', 'Quelques informations', 50, 5, 2, 2, 190000, 1, 'Carquefou', 'Rue Boucher d\'Argis', '44470', 0, '2019-12-19'),
+(3, 'Troidième bien', 'Quelques informations', 50, 5, 2, 2, 200000, 1, 'La Roche sur Yon', 'Place de La Vendée', '85000', 0, '2019-12-19'),
+(4, 'Quatrième bien', 'Quelques informations', 50, 5, 2, 2, 10000, 1, 'Venansault', 'rue des Alouettes', '85000', 0, '2019-12-19');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `migration_versions`
+--
+
+DROP TABLE IF EXISTS `migration_versions`;
+CREATE TABLE IF NOT EXISTS `migration_versions` (
+  `version` varchar(14) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `executed_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
